0d6563f523d83b855f1d20231ad67fe850d80222
GeorgeTheM3 <99905582+GeorgeTheM3@users.noreply.github.com>
2023-08-17 09:36:14 +0300

    view struct

---
+++ RickAndMorty/Views/CharacterInfoScreen/CharacterInfoViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  CharacterInfoViewModel.swift
+//  RickAndMorty
+//
+//  Created by Георгий Матченко on 17.08.2023.
+//
+
+import Foundation
---
+++ RickAndMorty/Views/MainScreen/MainViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  MainViewModel.swift
+//  RickAndMorty
+//
+//  Created by Георгий Матченко on 17.08.2023.
+//
+
+import Foundation
